8c1d2d8878bf83deaf7aa0fd06a2b2781f496254
weihung_lai <u9908028@gmail.com>
2022-03-14 11:48:27 +0800

    react-router-dom base route set

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -7,11 +7,15 @@ import Kura from "./kura/Kura";
 import Home from "./home/Home";
 import Tool from "./tool/Tool";
 
+//実際運用時のパスと開発時のパス設定、gh-pagesには実際運用時のパスが正常に作動しない
+const routerBasename = process.env.NODE_ENV === 'development' ? '/' : '/react-pages';
+
+
 //各項目とページとのRoute管理
 function Content(){
     return (
         <div>
-            <BrowserRouter>
+            <BrowserRouter basename= {routerBasename}>
                 <Routes>
                     <Route path="/about" element={<About/>} />
                     <Route path="/contact" element={<Contact/>} />
